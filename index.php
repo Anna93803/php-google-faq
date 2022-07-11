eb83b5c9f852aea1b994ff3ad8a15cf42d036568
Anna93803 <anna.tassone93@gmail.com>
2022-07-11 15:05:48 +0200

    add navbar

---
+++ index.php
@@ -0,0 +1,62 @@
+<?php
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="stylesheet" href="style.css">
+    <title>Faq Google</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="row">
+                <div class="col">
+                    <div class="logo-google">
+                        <img src="https://www.gstatic.com/images/branding/googlelogo/svg/googlelogo_clr_74x24px.svg" alt="">
+                    </div>
+                    <div class="termini">
+                        <span>Privacy e Termini</span>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="grid">
+                        <i class="fa-solid fa-grip"></i>
+                    </div>
+                    <div class="user">
+                        <img src="https://icon-library.com/images/avatar-icon-images/avatar-icon-images-6.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+
+            <div class="link">
+                <ul>
+                    <li><a href="#">Introduzione</a></li>
+                    <li><a href="#">Norme sulla privacy</a></li>
+                    <li><a href="#">Termini di servizio</a></li>
+                    <li><a href="#">Tecnologie</a></li>
+                    <li><a href="#">Domande Frequenti</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+
+    <main>
+        <div class="container">
+            
+        </div>
+    </main>
+</body>
+</html>
